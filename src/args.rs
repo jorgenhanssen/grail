917bbc479461fbcfba3536e6a693276e8312c220
Jørgen Hanssen <jorkiz97@gmail.com>
2024-12-25 18:38:22 +0100

    added args

---
+++ src/args.rs
@@ -0,0 +1,7 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "Grail")]
+#[command(author = "Jørgen Hanssen <jorgen@hanssen.io>")]
+#[command(version = "0.1.0")]
+pub struct Args {}
