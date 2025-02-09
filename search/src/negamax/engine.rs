a20b36d75143e30214f72929af86f81d2bc7b659
Jørgen Hanssen <jorkiz97@gmail.com>
2025-02-09 20:30:55 +0100

    use depth in q search correctly

--- search/src/negamax/engine.rs
+++ search/src/negamax/engine.rs
@@ -178,7 +178,7 @@ impl NegamaxEngine {
         }
 
         if depth >= max_depth {
-            return self.quiescence_search(board, alpha, beta, depth);
+            return self.quiescence_search(board, alpha, beta, 0);
         }
 
         let mut maybe_tt_move = None;
@@ -369,7 +369,7 @@ impl NegamaxEngine {
 
             self.position_stack.push(new_board.get_hash());
             let (child_score, mut child_line) =
-                self.quiescence_search(&new_board, -beta, -alpha, 0);
+                self.quiescence_search(&new_board, -beta, -alpha, depth + 1);
             self.position_stack.pop();
 
             let value = -child_score;
