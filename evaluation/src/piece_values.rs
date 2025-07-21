521cebb437fb8d3d0927c700a704522e80cb8154
Jørgen Hanssen <jorkiz97@gmail.com>
2025-07-21 19:27:49 +0200

    feat: delta pruning (#17)

--- evaluation/src/piece_values.rs
+++ evaluation/src/piece_values.rs
@@ -1,9 +1,11 @@
+use chess::ALL_PIECES;
+
 pub const PAWN_VALUE: i16 = 100;
 pub const KNIGHT_VALUE: i16 = 320;
 pub const BISHOP_VALUE: i16 = 330;
 pub const ROOK_VALUE: i16 = 500;
 pub const QUEEN_VALUE: i16 = 900;
-pub const KING_VALUE: i16 = 1000;
+pub const KING_VALUE: i16 = 0;
 
 #[inline(always)]
 pub fn piece_value(piece: chess::Piece) -> i16 {
@@ -17,7 +19,11 @@ pub fn piece_value(piece: chess::Piece) -> i16 {
     }
 }
 
-pub const PASSED_PAWN_BONUS: [i16; 8] = [0, 10, 20, 40, 80, 140, 220, 0];
-pub const ROOK_OPEN_FILE_BONUS: i16 = 15;
-pub const ROOK_SEMI_OPEN_FILE_BONUS: i16 = 8;
-pub const ROOK_ON_SEVENTH_BONUS: i16 = 20;
+#[inline(always)]
+pub fn total_material(board: &chess::Board) -> i16 {
+    let mut material = 0;
+    for piece in ALL_PIECES {
+        material += piece_value(piece) * (board.pieces(piece).popcnt() as i16);
+    }
+    material
+}
