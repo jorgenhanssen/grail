cee44d9e8fc143eb1405a9ecbafb02c9e0f360bd
Jørgen Hanssen <jorkiz97@gmail.com>
2025-07-21 13:29:16 +0200

    feat: evaluate castling (#16)
    * feat: evaluate castling
    
    * updated baselines

--- evaluation/src/def.rs
+++ evaluation/src/def.rs
@@ -2,5 +2,5 @@ use chess::Board;
 
 pub trait Evaluator {
     fn name(&self) -> String;
-    fn evaluate(&mut self, board: &Board) -> i16;
+    fn evaluate(&mut self, board: &Board, white_has_castled: bool, black_has_castled: bool) -> i16;
 }
