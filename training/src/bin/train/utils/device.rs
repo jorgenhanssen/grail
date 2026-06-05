969fac45ca74f89448b1e9b4f4ed2f08411c74c6
Jørgen Hanssen <jorkiz97@gmail.com>
2026-06-05 23:47:14 +0200

    reduce gemm f32 precision for nnue training

--- training/src/bin/train/utils/device.rs
+++ training/src/bin/train/utils/device.rs
@@ -5,6 +5,10 @@ pub fn get_device() -> Result<Device, Box<dyn Error>> {
     #[cfg(feature = "cuda")]
     if let Ok(device) = Device::cuda_if_available(0) {
         if device.is_cuda() {
+            // I found that reducing precision squeezes a bit of training speed.
+            // Since inference quantization is imprecise anyways, we might as well.
+            // https://developer.nvidia.com/blog/accelerating-ai-training-with-tf32-tensor-cores/
+            candle_core::cuda::set_gemm_reduced_precision_f32(true);
             return Ok(device);
         }
     }
